88ab08db40d15084c7e8e427d7b1f0cdc425921e
madhuri-khatal <madhuri.k@kitintellect.com>
2025-06-13 18:41:15 +0530

    clean code

--- src/main/java/com/ahmedabad/csr/controller/ProjectController.java
+++ src/main/java/com/ahmedabad/csr/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package com.ahmedabad.csr.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,15 +36,28 @@ public class ProjectController {
     return ResponseEntity.ok(projectService.updateProject(projetcId, project));
   }
 
-    @DeleteMapping("/deleteProject/{projetcId}")
-    public ResponseEntity<ApiResponse<String>> deleteProject(@PathVariable int projetcId) {
-        try {
-            // letestUpdateServices.deleteLetestUpdateById(projetcId);
-            projectService.deleteProject(projetcId);
-            ApiResponse<String> response = new ApiResponse<>(200, "project deleted successfully", null);
+  @DeleteMapping("/deleteProject/{projetcId}")
+  public ResponseEntity<ApiResponse<String>> deleteProject(@PathVariable int projetcId) {
+    try {
+      projectService.deleteProject(projetcId);
+      ApiResponse<String> response = new ApiResponse<>(200, "project deleted successfully", null);
+      return new ResponseEntity<>(response, HttpStatus.OK);
+    } catch (RuntimeException e) {
+      ApiResponse<String> response = new ApiResponse<>(404, e.getMessage(), null);
+      return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+  }
+
+   @GetMapping("/projectshowbyid/{letestupdateid}")
+    public ResponseEntity<ApiResponse<?>> getProjectById(@PathVariable int projetcId) {
+        Optional<Project> project = projectRepository.findById(projetcId);
+
+        if (project.isPresent()) {
+            ApiResponse<Project> response = new ApiResponse<>(200, "project fetched successfully",
+                    project.get());
             return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            ApiResponse<String> response = new ApiResponse<>(404, e.getMessage(), null);
+        } else {
+            ApiResponse<String> response = new ApiResponse<>(404, "letest Update not found", null);
             return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
     }
